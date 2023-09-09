7afec753c4de499a295140ecc3a5f116dd19e161
karan-patel61 <patel_karan_cool@hotmail.com>
2023-09-09 00:02:45 -0400

    folder restructure

---
+++ app/components/navbar.tsx
@@ -0,0 +1,22 @@
+import React from "react"
+
+import Link from "next/link";
+export default function Navbar() {
+  return (
+    <nav className="relative flex w-full flex-wrap items-center bg-gray-900 text-white py-3 px-3 shadow-lg lg:flex-wrap lg:justify-start">
+    <ul className="flex px-3 space-x-4 items-center">
+      <li className="transition ease-in-out delay-50 hover:bg-gray-700 p-2 rounded-md">
+          <Link href="/">Home</Link>
+        </li>
+        <li className="hover:bg-gray-700 p-2 rounded-md">
+          <Link href="/dashboard">Dashboard</Link>
+        </li>
+        <li>
+        <button className="p-2 bg-sky-500 rounded-md text-white" type="button">Sign In</button>
+        </li>
+    </ul>
+    
+    </nav>
+    
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen outline flex-col items-center justify-between p-16">
+      <div className="flex items-center outline justify-between p-16 ">
+      <p className="relative flex place-items-center"> Welcome to BDrive Homepage!</p>
+      </div>
+    </main>
+  )
+}
