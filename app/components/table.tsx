b1fc5d8852b95477e0c20bdbab28ed5da09bdfd2
karan-patel61 <patel_karan_cool@hotmail.com>
2024-01-26 19:18:15 -0500

    centered delete loading spinner

--- app/components/table.tsx
+++ app/components/table.tsx
@@ -2,6 +2,7 @@
 import { Session, createClientComponentClient} from '@supabase/auth-helpers-nextjs'
 import { Database } from '../../supabase'
 import { useEffect, useState } from 'react';
+import { list } from 'postcss';
 
 type Todos = Database['public']['Tables']['todos']['Row']
 
@@ -41,27 +42,16 @@ export default function Table({ session }: { session: Session | null }){
       return Math.round(bytes / Math.pow(1024, i)) + ' ' + sizes[i];
    }
     const listFiles = todos.map(file =>
-        <tr className='hover:bg-gray-100/50 text-center' key={file.user_id}>
+        <tr className='bg-blur-sm text-center hover:bg-gray-100/10' key={file.user_id}>
             <td>{file.file_name}</td>
             <td>{file.inserted_at}</td>
             <td>{bytesToSize(file.file_size?file.file_size:0)}</td>
-            {deleteFile?
-            (
-              <div className='flex justify-center items-center space-x-2 mt-1'>
-                <p>Deleting...</p>
-                <span className="small-loader"></span>
-              </div>
-              )
-            :
             <td>
               <button onClick={e => deleteTodo(file.id, file.file_name)}> Delete</button>
               <form action={"/api/download/"+file.file_name} method="get" className='inline'>              
                 <button id='download' type='submit'> Down</button>
               </form>
             </td>
-            }
-            
-            
         </tr>
       );
 
@@ -83,20 +73,41 @@ export default function Table({ session }: { session: Session | null }){
       }, [supabase,todos])
       
     return(
-        <div className="bg-transparent p-4 rounded-md inline-block flex w-full shadow justify-center">
-            <table className="table-auto w-4/5 items-center rounded-md outline">
-                <thead className='border-b-2 border-neutral-300'>
-                    <tr>
-                    <th>File Name</th>
-                    <th>Upload Date</th>
-                    <th>Size</th>
-                    <th>Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {listFiles}
-                </tbody>
+        <div className="backdrop-blur-sm bg-white/20 rounded-xl h-full w-full p-4 rounded-md inline-block flex flex-col mt-3 justify-center items-center">
+          { deleteFile ? 
+            <div className='w-full'>
+              <table className="table-auto w-full items-center rounded-md">
+              <thead className='border-b-2 border-neutral-300'>
+                  <tr>
+                  <th>File Name</th>
+                  <th>Upload Date</th>
+                  <th>Size</th>
+                  <th>Actions</th>
+                  </tr>
+              </thead>
+              </table>
+              <div className='flex justify-center space-x-2 p-2 w-full'>
+                <p>Deleting...</p>
+                <span className="loader"></span>
+              </div>
+            </div>
+          :
+            <table className="table-auto w-full items-center rounded-md">
+              <thead className='border-b-2 border-neutral-300'>
+                  <tr>
+                  <th>File Name</th>
+                  <th>Upload Date</th>
+                  <th>Size</th>
+                  <th>Actions</th>
+                  </tr>
+              </thead>
+              <tbody>
+                {listFiles}
+              </tbody>
             </table>
+          }
+            
+            
         </div> 
     )
 }
