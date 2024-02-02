e959c3784f60176393ac59a0eeb6ef828a848cea
karan-patel61 <patel_karan_cool@hotmail.com>
2024-02-01 21:56:19 -0500

    added user avatar

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,6 +3,7 @@ import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { Database } from '../../supabase'
 import { cookies } from 'next/headers'
 import Upload from "../components/file-upload";
+import Image from "next/image";
 
 
 // `app/dashboard/page.tsx` is the UI for the `/dashboard` URL
@@ -19,24 +20,28 @@ export default async function Page() {
 
     return (
       <section className="bg-gray-900 flex-col h-screen text-white">
-            <div className="flex py-5 items-center justify-center">
-              <div className="flex flex-row items-center space-x-4">
+            <div className="flex py-4 items-center justify-center">
+              <div className="flex flex-row items-center space-x-4 shadow-lg shadow-purple-700/50 rounded-full p-2 outline outline-1 outline-gray-700">
                 <h1
                     className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl"
                 >
                     Dashboard
                 </h1>
+                <Image
+                  src={session?.user.user_metadata.avatar_url}
+                  width={50}
+                  height={50}
+                  className="rounded-full place-self-end "
+                  alt="User Avatar"
+                />
                 <form action="/auth/signout" method="post">
-                      <button className="button p-2 bg-red-600 rounded-md text-white" type="submit">
+                      <button className="button p-3 bg-red-600 rounded-full text-white" type="submit">
                         Sign out
                       </button>
                 </form>
               </div>    
             </div>
-            <div>
-
-            </div>
-            <div className="flex flex-col items-center justify-between">
+            <div className="flex flex-col items-center justify-between mt-3 ">
               <div>
                  <Upload session={session}/>   
               </div> 
