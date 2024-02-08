619ee4ad56b18c60ff7851225b57810e2b72ecb7
karan-patel61 <patel_karan_cool@hotmail.com>
2024-02-07 20:55:32 -0500

    revert back to localhost url

--- app/components/auth-form.tsx
+++ app/components/auth-form.tsx
@@ -20,7 +20,7 @@ export default function AuthForm() {
         }
       }}
       providers={['google']}
-      redirectTo="https://bdrive-app.vercel.app/auth/callback"
+      redirectTo="http://localhost:3000/auth/callback"
     />
   )
 }
