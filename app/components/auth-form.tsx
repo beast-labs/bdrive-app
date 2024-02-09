f8feb0e09b7998728a58571a62d455c79d467686
karan-patel61 <patel_karan_cool@hotmail.com>
2024-02-08 22:30:53 -0500

    corrected url

--- app/components/auth-form.tsx
+++ app/components/auth-form.tsx
@@ -18,7 +18,7 @@ export default function AuthForm() {
         }
       }}
       providers={['google']}
-      redirectTo='https://bdrive-app.vercel.app/auth/callback'
+      redirectTo="https://bdrive-app.vercel.app/auth/callback"
     />
   )
 }
