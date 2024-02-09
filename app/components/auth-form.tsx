d03d4f75af1f29511a5bdc487fddc802cec7e08d
karan-patel61 <patel_karan_cool@hotmail.com>
2024-02-08 22:20:02 -0500

    Footer Year & Table Sync (#9)
    * title change
    
    * fixed table sync issue

--- app/components/auth-form.tsx
+++ app/components/auth-form.tsx
@@ -3,10 +3,8 @@ import { Auth } from '@supabase/auth-ui-react'
 import { ThemeSupa } from '@supabase/auth-ui-shared'
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 import { Database } from '../../supabase'
-// import { createClient } from '@supabase/supabase-js'
 
 export default function AuthForm() {
-  // const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL,process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
   const supabase = createClientComponentClient<Database>()
   return (
     <Auth
@@ -20,7 +18,7 @@ export default function AuthForm() {
         }
       }}
       providers={['google']}
-      redirectTo='https://bdrive-app.vercel.app/auth/callback'
+      redirectTo='http://localhost:3000/auth/callback'
     />
   )
 }
--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,11 +1,10 @@
 export default function Navbar() {
+  let year = new Date().getFullYear();
     return (
         <footer className="bg-gray-800 text-center text-white dark:bg-neutral-600">
-    
-        {/* <!--Copyright section--> */}
         <div
           className="p-4 text-center text-neutral-400 dark:bg-neutral-700 dark:text-neutral-200">
-          © 2023 Copyright:
+          © {year} Copyright:
           <a
             className=""
             href="https://github.com/beast-labs/bdrive-app"
