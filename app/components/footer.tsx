e82f78b047c38fff77763a79ab8bd63cf83d6277
karan-patel61 <patel_karan_cool@hotmail.com>
2024-02-09 20:20:56 -0500

    footer flex and w-full

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,9 +1,9 @@
 export default function Navbar() {
   let year = new Date().getFullYear();
     return (
-        <footer className="bg-gray-800 text-center text-white dark:bg-neutral-600 w-full">
+        <footer className="flex bg-gray-800 text-center text-white dark:bg-neutral-600">
         <div
-          className="p-4 text-center text-neutral-400 dark:bg-neutral-700 dark:text-neutral-200">
+          className="p-4 text-center text-neutral-400 dark:bg-neutral-700 dark:text-neutral-200 w-full">
           © {year} Copyright:
           <a
             className=""
