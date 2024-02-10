22a5d360cb7ebbfa459e84e4dd4b1f81c93fc21c
karan-patel61 <patel_karan_cool@hotmail.com>
2024-02-10 10:28:15 -0500

    remove footer outline

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,7 +1,7 @@
 export default function Navbar() {
   let year = new Date().getFullYear();
     return (
-        <footer className="w-full bg-gray-800 text-center text-white dark:bg-neutral-600 outline">
+        <footer className="w-full bg-gray-800 text-center text-white dark:bg-neutral-600">
         <div
           className="p-4 text-center text-neutral-400 dark:bg-neutral-700 dark:text-neutral-200 w-full">
           © {year} Copyright:
